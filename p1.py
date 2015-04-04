5ac651c2e53f0e3943025fc9fb3eac18de931db5
cdilday <cdilday@ucsc.edu>
2015-04-04 16:57:23 -0700

    Got the algorithm running and saving distances, now it just needs to detect when it's found dst and then rebuild that path and return it. For now it processes everything in that room, then returns nothing and therefore says it's impossible. Will continue work on it later tonight, I'm just pushing because it's a checkpoint

--- p1.py
+++ p1.py
@@ -4,10 +4,46 @@ from math import sqrt
 from heapq import heappush, heappop
 
 def dijkstras_shortest_path(src, dst, graph, adj):
-	raise NotImplementedError	
+	Dist = {}
+	Prev = {}
+	#and another one to check if nodes have been fully processed
+	Processed = {}
+	Dist[src] = 0
+	queue = [(0,src)]
+	
+	#while queue is not empty. If it's empty we've searched all valid points
+	while(queue):
+		curr = heappop(queue)
+		Processed[curr[1]] = True
+		#print "Popping %s with Distance" %(curr[1],)
+		nextCells = navigation_edges(graph, curr[1])
+		for i in range(0, len(nextCells)):
+			if not (nextCells[i][1] in Processed):
+				#update the distance in the current tuple
+				nextCells[i] = (nextCells[i][0] + Dist[curr[1]], nextCells[i][1])
+				#check if distance needs updating in the dict
+				if not (nextCells[i][1] in Dist) or (Dist[nextCells[i][1]] > nextCells[i][0]):
+					Dist[nextCells[i][1]] = nextCells[i][0]
+					Prev[nextCells[i][1]] = curr[1]
+					#push onto the queue
+					heappush(queue, nextCells[i])
+			
+		
+		
+		
 
 def navigation_edges(level, cell):
-	raise NotImplementedError
+	validCells = []
+	x, y = cell
+	for dx in [-1,0,1]:
+		for dy in [-1,0,1]:
+			next_cell = (x + dx, y + dy)
+			dist = sqrt(dx*dx+dy*dy)
+			if dist > 0 and next_cell in level['spaces']:
+				validCells.append((dist, next_cell))
+				
+	return validCells
+	
 
 def test_route(filename, src_waypoint, dst_waypoint):
 	level = load_level(filename)
